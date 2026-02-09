32c44a3bbdb456d26d4ceb3a56b33cea144691c6
Abhishek <abhishek.kabi.21@gmail.com>
2026-02-09 12:36:10 +0530

    Added new features

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -2161,7 +2161,7 @@ function Calendar() {
                             //     ? "0 0 10px red, 0 0 20px rgba(255, 255, 255, 0.5)"
                             //     : "none",
                             // fontWeight: "bold",
-                            fontSize: "20px",
+                            fontSize: "16px",
                             // backgroundColor: getEventColorForDate(date),
                             cursor: "pointer",
                             ...borderStyle,
