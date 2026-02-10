f0df4d2eb2ee1de80a198072d9832bab3cd06ca0
Abhishek <abhishek.kabi.21@gmail.com>
2026-02-10 21:42:46 +0530

    Added new features

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1163,7 +1163,7 @@ function Calendar() {
                           backgroundColor: isDarkTheme ? "black" : "white",
                           border: isDarkTheme
                             ? "1px solid #1a1a1a"
-                            : "1px solid rgb(239, 239, 239)",
+                            : "1px solid rgb(242, 242, 242)",
 
                           padding: startIndex === 0 ? "5px" : "10px",
 
